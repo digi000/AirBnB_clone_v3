9df863e148b02363e63fc9f6e5e829ef2fb694e7
Tuyisenge Jordan <jordanale.112@gmail.com>
2024-05-26 22:59:26 +0200

    refixed task 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
@@ -8,6 +7,11 @@ from os import getenv
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    storage.close()
 
 
 if __name__ == "__main__":
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-
-from api.v1.views import app_views
 from flask import jsonify
 from api.v1.views import app_views
 
 
-@app_views.route('/status')
-def get_status():
-    response = {'status': "OK"}
-    return jsonify(response)
+@app_views.route('/status', methods=['GET'])
+def api_status():
+    cvb = {'status': "OK"}
+    return jsonify(cvb)
