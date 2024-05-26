0a4d4d0765ec4c39528a8aa645312980c4d0722b
Tuyisenge Jordan <jordanale.112@gmail.com>
2024-05-26 16:57:24 +0200

    in  api/v1/app.py imported os and register the blueprint app_views to your Flask instance app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+import os
 
 app = Flask(__name__)
+app.register_blueprint(app_views)
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     storage.close()
