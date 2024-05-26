06132148e2334a5a1d289aeeb7ab7cd0cf913977
Tuyisenge Jordan <jordanale.112@gmail.com>
2024-05-26 17:26:52 +0200

    inside files added #! /usr/bin/python3

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 from flask import Blueprint
 from flask import Blueprint
 from flask import Blueprint
@@ -5,5 +7,3 @@ from api.v1.views.index import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-# Add more imports for other view modules if needed
