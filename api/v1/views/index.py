945dec558c690900ebf2e5f82d785bc43b87503c
Tuyisenge Jordan <jordanale.112@gmail.com>
2024-05-27 16:23:43 +0200

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3
 from flask import jsonify
 from api.v1.views import app_views
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
 def api_status():
     cvb = {'status': "OK"}
     return jsonify(cvb)
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(stats)
